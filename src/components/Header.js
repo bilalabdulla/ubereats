b18c99bbdce1cd2776adae174ca559e95ca0ef16
bilalabdulla <bilalabdullaav9@gmail.com>
2024-06-24 16:32:11 +0530

    second commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Header() {
+
+  const [modal, setModal] = useState(false)
+
+  const toggleModal = () => {
+    setModal(!modal)
+  }
+
+  if (modal) {
+    document.body.classList.add('active-modal')
+  } else {
+    document.body.classList.remove('active-modal')
+  }
+
   return (
+    <header>
     <div className='header'>
       <h1 className='title-one'>Order delivery near you</h1>
       <div className='header-mid'>
@@ -11,9 +25,9 @@ function Header() {
         </div>
         <div className='delivery'>
             <i class="fa-solid fa-clock delivery-icon"></i>
-            <select className='delivery-select'>
+            <select className='delivery-select' onChange={toggleModal}>
                 <option className='option'>Deliver now</option>
-                <option className='option'>Schedule for later</option>
+                <option onClick={toggleModal} className='option btn-modal'>Schedule for later</option>
             </select>
         </div>
         <button className='search-btn'>Search here</button>
@@ -23,6 +37,36 @@ function Header() {
         <a>Sign in</a>
       </div>
     </div>
+    
+    {modal && (
+        <div className='modal'>
+        <div className='overlay' onClick={toggleModal}></div>
+        <div className='modal-content'>
+          <h2 className='modal-title'>Pick a time</h2>
+          <select className='modal-select'>
+            <option>June 24</option>
+            <option>June 25</option>
+            <option>June 26</option>
+            <option>June 27</option>
+            <option>June 28</option>
+            <option>June 29</option>
+            <option>June 30</option>
+          </select>
+          <select className='modal-select'>
+            <option>5 AM</option>
+            <option>6 AM</option>
+            <option>7 AM</option>
+            <option>8 AM</option>
+            <option>9 AM</option>
+            <option>10 AM</option>
+          </select>
+          <button className='modal-in-btn'>Schedule</button>
+          <button className='modal-in-btn-two'>Deliver now</button>
+          <button className='close-modal' onClick={toggleModal}>X</button>
+        </div>
+      </div>
+    )}
+    </header>
   )
 }
 export default Header
